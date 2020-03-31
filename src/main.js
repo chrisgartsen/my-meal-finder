fa9310bac83d84bc192fe95017a3fd35cb9416d9
Chris Gartsen <chris@gartsen.nl>
2020-03-31 17:18:01 +0200

    Add SCSS support

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './registerServiceWorker'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./registerServiceWorker";
 
-Vue.config.productionTip = false
+import "./assets/scss/main.scss";
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
